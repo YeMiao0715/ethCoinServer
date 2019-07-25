345e42c7385c9be232f4035c9fd62c865fca7729
YeMiao <yemiao0715@163.com>
2019-07-25 18:41:15 +0800

    构建http服务

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,13 +1,44 @@
+import { LogModel } from './../model/LogModel';
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
 import router from './router';
 const app = new Koa();
 
+/**
+ * 检测安全组
+ * @param {string} ip
+ * @returns
+ */
+function checkIp(ip: string) {
+  let status = false;
+  let ipString = process.env.HTTP_SERVER_IP_LIST;
+  if(ipString.includes(',')){
+    const ipList = ipString.split(',');
+    ipList.map(value => {
+      if(ip === value) {
+        status = true
+      }
+    });
+  }else{
+    if(ip === ipString) {
+      status = true;
+    }else {
+      status = false;
+    }
+  }
+  return status;
+}
+
+
+
 app.use(async (ctx, next) => {
   // 安全组检测
-  let status = false;
-  console.log(process.env.HTTP_SERVER_IP_LIST)
+  if(checkIp(ctx.ip) === false) {
+    ctx.throw(403, '权限不足');
+  }
 
+  LogModel.info(`${ctx.method} ${ctx.url}`, LogModel.SCENE_HTTPS_INFO, ctx.query);
+  
   // 设置响应头
   ctx.set('Content-Type', 'application/json;charset: UTF-8');
   await next();
@@ -17,4 +48,4 @@ app.use(bodyParser());
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-export default app;
+export = app;
--- src/router.ts
+++ src/router.ts
@@ -2,4 +2,8 @@ import Router from 'koa-router';
 
 const router = new Router();
 
+router.head('/ipCheck', (ctx) => {
+  ctx.body = {status: 'ok'};
+})
+
 export default router;
